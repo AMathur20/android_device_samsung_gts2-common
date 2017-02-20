0abfa987af1cf7ad0520072e1e173ef94d614d9a
Anks329 <anks329@hotmail.com>
2017-02-19 21:54:25 -0500

    Bring back AdvancedDisplay
    Signed-off-by: Anks329 <anks329@hotmail.com>

--- board_defines/sec-cmhw.mk
+++ board_defines/sec-cmhw.mk
@@ -2,4 +2,4 @@ DEVICE_PATH := device/samsung/gts2-common
 
 # Hardware
 BOARD_HARDWARE_CLASS += $(DEVICE_PATH)/cmhw
-#BOARD_HARDWARE_CLASS += hardware/samsung/cmhw
+BOARD_HARDWARE_CLASS += hardware/samsung/cmhw
